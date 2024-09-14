137bf294dc5b286c8ad36022c31c648b62358ce1
Henry <mbahenryemeka@gmail.com>
2024-09-14 02:22:22 -0400

    more robust code for lunch.js

--- lunch.js
+++ lunch.js
@@ -11,29 +11,28 @@
  * hungry is a Boolean, representing if you're hungry or not.
  * availableTime is a Number representing the time you have for lunch,
  * in minutes.
- */
-
-const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
-}
+*/
 
 
-/*
- * This is some test runner code that's simply calling our whatToDoForLunch function
- * defined above to verify we're making the right decisions. Do not modify it!
- */
+const whatToDoForLunch = function (hungry, availableTime) {
+  if (!hungry) {
+    console.log("Let's get back to work.");
+    return;
+  };
 
-console.log("I'm hungry and I have 20 minutes for lunch.");
-whatToDoForLunch(true, 20);
-console.log("---");
+  if (availableTime < 0) {
+    console.log("Time cannot be negative.");
+    return;
+  }
 
-console.log("I'm hungry and I have 50 minutes for lunch.");
-whatToDoForLunch(true, 50);
-console.log("---");
+  if (hungry && availableTime < 20) {
+    console.log("Let's pick something up and eat it in the lab.");
+  } else if (hungry && availableTime <= 30) {
+    console.log("Let's try a place nearby");
+  } else {
+    console.log("We're in a bootcamp, so we should reconsider how much time we actually have to spare.")
+  }
+};
 
-console.log("I'm not hungry and I have 30 minutes for lunch.");
-whatToDoForLunch(false, 30);
-console.log("---");
 
-console.log("I'm hungry and I have 15 minutes for lunch.");
-whatToDoForLunch(true, 15);
+
